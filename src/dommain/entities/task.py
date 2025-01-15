f07a638911cc17cd5035553b328854ea70ec8ec0
Marck Carrión Guzmán <mcarrion@kcramsolutions.com>
2025-01-15 14:53:04 +0100

    feat: agregado owner y watchers a la tarea

--- src/dommain/entities/task.py
+++ src/dommain/entities/task.py
@@ -8,9 +8,16 @@ class TaskEntity:
     end_date = datetime
     project_id = str
     metadata = {}
+    watchers = []
 
     def set_metadata(self, metadata: dict):
         self.metadata = metadata
     
     def add_meta(self, key: str, value):
         self.metadata[key] = value
+
+    def set_watchers(self, watchers):
+        self.watchers = watchers
+    
+    def add_watcher(self, watcher):
+        self.watchers.append(watcher)
